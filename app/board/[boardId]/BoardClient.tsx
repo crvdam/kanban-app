f600dd3cd5aefa78bef59576c2796e033e81ee55
crvd <casparvandam@hotmail.com>
2026-03-04 21:52:40 +0100

    added 'create card' button and api route

--- app/board/[boardId]/BoardClient.tsx
+++ app/board/[boardId]/BoardClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query";
-import type { Board, Column } from "../../types/index";
+import type { Board, Column, Card } from "../../types/index";
 import styles from "./BoardClient.module.css";
 
 export default function BoardClient({ boardId }: { boardId: string }) {
@@ -33,17 +33,45 @@ export default function BoardClient({ boardId }: { boardId: string }) {
     },
   });
 
-  const handleCreate = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleCreateColumn = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     createColumn.mutate();
   };
 
+  const deleteColumn = useMutation({
+    mutationFn: async (columnId: string) => {
+      const results = await fetch(`/api/columns/${columnId}`, {
+        method: "DELETE",
+      });
+      if (!results.ok) throw new Error("Faled to delete column");
+      return results.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["board"] });
+    },
+  });
+
+  const createCard = useMutation({
+    mutationFn: async (columnId: string) => {
+      const result = await fetch(`/api/columns/${columnId}`, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ name: undefined }),
+      });
+
+      if (!result.ok) throw new Error("Failed to create card");
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["board"] });
+    },
+  });
+
   return (
     <div>
       <h1>Dashboard</h1>
 
-      <form onSubmit={handleCreate}>
+      <form onSubmit={handleCreateColumn}>
         <button type="submit" disabled={createColumn.isPending}>
           {createColumn.isPending ? "Creating..." : "Create new column"}
         </button>
@@ -53,8 +81,18 @@ export default function BoardClient({ boardId }: { boardId: string }) {
         {board?.columns?.map((column: Column) => (
           <div className={styles.column} key={column.id}>
             <h3>{column.name}</h3>
-            <button>Add task</button>
-            <button>Remove column</button>
+            <button onClick={() => createCard.mutate(column.id)}>
+              Add task
+            </button>
+            <button onClick={() => deleteColumn.mutate(column.id)}>
+              Remove column
+            </button>
+
+            {column.cards?.map((card: Card) => (
+              <div key={card.id}>
+                <p>Card</p>
+              </div>
+            ))}
           </div>
         ))}
       </div>
