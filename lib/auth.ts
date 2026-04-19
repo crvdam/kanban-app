12679e0437dfb331750b8d4ed363255a2ed94627
crvdam <casparvandam@gmail.com>
2026-04-19 18:08:13 +0200

    netlify specific settings

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,44 +1,45 @@
-import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
-import { prisma } from "@/lib/prisma"
-import bcrypt from "bcryptjs"
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma";
+import bcrypt from "bcryptjs";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  session: { strategy: "jwt" },
-  callbacks: {
-    jwt({ token, user }) {
-      if (user) token.id = user.id
-      return token
+    trustHost: true,
+    session: { strategy: "jwt" },
+    callbacks: {
+        jwt({ token, user }) {
+            if (user) token.id = user.id;
+            return token;
+        },
+        session({ session, token }) {
+            if (token.id) {
+                session.user.id = token.id as string;
+            }
+            return session;
+        },
     },
-    session({ session, token }) {
-      if (token.id) {
-        session.user.id = token.id as string
-      }
-      return session
-    }
-  },
-  providers: [
-    Credentials({
-      credentials: {
-        email: { label: "Email", type: "email" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) return null
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email as string }
-        })
-        if (!user) return null
-        const passwordMatch = await bcrypt.compare(
-          credentials.password as string,
-          user.password
-        )
-        if (!passwordMatch) return null
-        return user
-      }
-    })
-  ],
-  pages: {
-    signIn: "/login"
-  }
-})
+    providers: [
+        Credentials({
+            credentials: {
+                email: { label: "Email", type: "email" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) return null;
+                const user = await prisma.user.findUnique({
+                    where: { email: credentials.email as string },
+                });
+                if (!user) return null;
+                const passwordMatch = await bcrypt.compare(
+                    credentials.password as string,
+                    user.password,
+                );
+                if (!passwordMatch) return null;
+                return user;
+            },
+        }),
+    ],
+    pages: {
+        signIn: "/login",
+    },
+});
