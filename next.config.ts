6fb4e951427d4e724813bf03cb49380332def5d8
crvdam <casparvandam@gmail.com>
2026-04-19 19:10:04 +0200

    /api/ config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig = {
     outputFileTracingIncludes: {
         "/**": ["./app/generated/prisma/**"],
+        "/api/**": ["./app/generated/prisma/**"],
     },
 };
 
